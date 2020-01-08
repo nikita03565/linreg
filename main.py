54eaf0454fa1ba21eda4b956aa62acfe2a7ab166
nikita <nikita03565@gmail.com>
2020-01-08 21:23:39 +0300

    updated check predictions

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from helpers import generate_data, LinearRegression
 # data range and number of points
 l_bound = 0
 r_bound = 100
-n = 1000
+n = 100
 data = generate_data(l_bound, r_bound, n)
 
 linreg = LinearRegression()
@@ -16,11 +16,13 @@ xx = np.linspace(l_bound, r_bound, n)
 yy = np.array(linreg.b[0] + linreg.b[1] * xx)
 
 # Check predictions
-pred_x = [[10], [33], [100], [50], [60], [70]]
+check_data = generate_data(l_bound, r_bound, n // 10)
+pred_x = [[x] for x in check_data[:, 0]]
 pred = linreg.predict(pred_x)
 
 plt.figure(1)
 plt.plot(xx, yy.T, color='b')
 plt.scatter(data[:, 0], data[:, 1], color='r')
-plt.scatter(pred_x, pred, color='g')
+plt.scatter(check_data[:, 0], check_data[:, 1], color='g')
+plt.scatter(pred_x, pred, color='c')
 plt.show()
